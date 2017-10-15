07a9e799c287ba925d0a6930a45272a3f50a61cb
josecarlosjr <jose.carlosjr@outlook.com>
2017-10-14 23:53:03 -0300

    Update my_module.py

--- my_module.py
+++ my_module.py
@@ -7,3 +7,9 @@ if __name__ == "__main__":
     print("Executando o programa main")
     print("O valor de __name__ é: ", __name__)
     hello()
+    
+    
+    #EXECUTING THE FILE YOU WILL HAVE
+    #> Executando o programa main
+    #> O valor de __name__ é: __main__
+    #> i am from my_module.py
