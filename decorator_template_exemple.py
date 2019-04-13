d8534e23c2087034f0c32c3c11a5d7fa4180a7f4
josecarlosjr <jose.carlosjr@outlook.com>
2019-04-13 19:33:51 -0300

    Create decorator_template_exemple.py

---
+++ decorator_template_exemple.py
@@ -0,0 +1,37 @@
+#Note:The template file will be copied to a new file. When you change the code of the template file you can create new file with this base code. 
+#entendendo um decorator
+# fonte udemy
+
+import sys
+import os
+import functools #cria um decorator atraves do functools 
+
+# um decorator é uma funcao que chama outra funcao
+
+#2- o decorator @my_decorator ira chamar
+# a funcao def my_decorator passando
+#a funcao def my_function como argumento
+# ou seja my_function = func logo em seguida
+# ira chamar a funcao que roda func
+
+def my_decorator(func):
+    
+   @functools.wraps(func)
+   def function_that_runs_func():
+       
+       print("antes da funcao")#primeiro
+       func()
+       print("depois da funcao")#terceiro
+       return function_that_runs_func
+
+
+#1- ao criar uma funcao com um
+#decorator ela irar executar primeiro
+#a funcao my_decorator
+
+@my_decorator
+def my_function():
+    
+    print("my function")# segundo
+   
+my_function()
