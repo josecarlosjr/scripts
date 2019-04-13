608844c7483a83b63a4be5edb104736fa5d476da
josecarlosjr <jose.carlosjr@outlook.com>
2019-04-13 19:44:30 -0300

    Update decorator_advanced_exemple.py
    exemple of decorator advanced template

--- decorator_advanced_exemple.py
+++ decorator_advanced_exemple.py
@@ -1,13 +1,15 @@
 #este codigo mostra o uso de decorators avancados
 # decorators avancados podem passar argumentos
-
+#diferentemente de um decorator simples, um decorator
+#irá ter mais funções aninhadas mas não mais do que 3 funções aninhadas
 import sys
 import functools
 
-def decorator_with_arguments(number):
-    def my_decorator(func):
-        @functools.wraps(func)
+def decorator_with_arguments(number):    
+    def my_decorator(func):        
+        @functools.wraps(func)        
         def function_that_runs_func():
+            
             print("in the decorator")
             if number == 56:
                 print("o argumento é 56")
@@ -21,5 +23,7 @@ def decorator_with_arguments(number):
 @decorator_with_arguments(56)
 def my_function():
     print("hello")
-    
+
+#do mesmo modo que o decorator simples
+#a funcao abaixo irá invocar o decorator decorator_with_arguments()
 my_function()
